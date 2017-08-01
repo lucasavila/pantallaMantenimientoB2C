0687281f77dbc07d287727821e145e738192a423
MatiasDG <matiasdanielgarcia@gmail.com>
2017-08-01 18:17:38 -0300

    Fix image

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
       }
-    }, 
+    },
 
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
@@ -261,7 +261,9 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+/*
+          '<%= yeoman.dist %>/images/{,*!/}*.{png,jpg,jpeg,gif,webp,svg}',
+*/
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
