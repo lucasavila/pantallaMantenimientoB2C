a2ac066d0ef0e16872829a7f26cd99a262429857
Lucas Avila <lucas.avila.2010@gmail.com>
2017-08-01 16:23:04 -0300

    run para la img en preprod o prod

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+docker stop pantalla-mantenimiento
+docker rm pantalla-mantenimiento
+
 docker run --name pantalla-mantenimiento -p 8888:80 docker.dev.redbee.io/pantalla-mantenimiento-sep
