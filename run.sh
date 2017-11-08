4d8868e22313b07cba510a6e375ee5863320f59e
Lucas Avila <lucas.avila.2010@gmail.com>
2017-11-07 21:17:12 -0300

    se cambia puerto y se agrega docker de b2c

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker rmi docker.dev.redbee.io/pantalla-mantenimiento-b2c
 docker stop pantalla-mantenimiento-b2c
 docker rm pantalla-mantenimiento-b2c
 
-docker run --name pantalla-mantenimiento -p 8889:80 -d docker.dev.redbee.io/pantalla-mantenimiento-b2c
+docker run --name pantalla-mantenimiento-b2c -p 8889:80 -d docker.dev.redbee.io/pantalla-mantenimiento-b2c
