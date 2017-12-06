88530c2bf686b1e4cc54ce7394867edd12e760b6
Lucas Avila <lucas.avila.2010@gmail.com>
2017-12-05 21:59:12 -0300

    git jenkins

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 docker rmi docker.dev.redbee.io/pantalla-mantenimiento-b2c
+
 docker stop pantalla-mantenimiento-b2c
+
 docker rm pantalla-mantenimiento-b2c
 
 docker run --name pantalla-mantenimiento-b2c -p 8889:80 -d docker.dev.redbee.io/pantalla-mantenimiento-b2c
